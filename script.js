408f69626aab7925e6e0ae6450b125b9cb65dbe3
katesereda1808 <katesereda22@gmail.com>
2022-06-27 16:46:25 +0300

    design fixed

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ burger.addEventListener('click', function(){
 close_menu.addEventListener('click', function(){
   mobile_menu.classList.add('hidden');
 });
+
+let slides = document.querySelectorAll('.slide');
+function hideSlides(){
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove('shown')
+    slides[i].classList.add('hidden')
+  };
+}
+function showSlide(i){
+    // slides[i].classList.remove('hidden');
+    slides[i].classList.replace('hidden', 'shown')
+}
+
+function getWidth(){
+  let width = document.documentElement.clientWidth;
+  if(width<=900){
+    hideSlides();
+    showSlide(0);
+    // return 'mobile';
+  } else {
+    for (i = 0; i < slides.length; i++) {
+      slides[i].classList.remove('hidden');
+      slides[i].classList.remove('shown');
+    }
+    // return 'desktop';
+  }
+};
+getWidth();
+window.addEventListener('resize', getWidth);
