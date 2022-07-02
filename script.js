4fe1a6b82374da01e1908754471171f41757197f
katesereda1808 <katesereda22@gmail.com>
2022-07-02 23:23:13 +0200

    errors fixed, header slider added

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 let mobile_menu = document.querySelector('.header_navbar_mobile');
 let close_menu = document.querySelector('.burger_close');
 let burger=document.querySelector('.menu_btn_mobile');
-console.log(burger)
-// burger.addEventListener('click', function(){
-//   mobile_menu.classList.remove('hidden');
-// });
-// close_menu.addEventListener('click', function(){
-//   console.log('close')
-//   mobile_menu.classList.add('hidden');
-// });
+let header_chevron_left = document.querySelector('.arrow_left');
+let header_chevron_right = document.querySelector('.arrow_right');
+let header_brands = document.querySelector('.header_brands');
+function scroll(direction) {
+  switch (direction) {
+    case "right":
+      header_brands.scrollLeft += 100;
+      break;
+    case "left":
+      header_brands.scrollLeft -= 100;
+      break;
+    default:
+      break;
+  }
+}
+header_chevron_left.addEventListener('click', ()=>scroll('left'));
+header_chevron_right.addEventListener('click', ()=>scroll('right'));
+burger.addEventListener('click', function(){
+  mobile_menu.classList.remove('hidden');
+});
+close_menu.addEventListener('click', function(){
+  mobile_menu.classList.add('hidden');
+});
 
 let slides = document.querySelectorAll('.slide');
 function hideSlides(){
@@ -17,24 +32,25 @@ function hideSlides(){
     slides[i].classList.add('hidden')
   };
 }
+
 function showSlide(i){
-    // slides[i].classList.remove('hidden');
     slides[i].classList.replace('hidden', 'shown')
 }
 
-function getWidth(){
+if(document.querySelector('.slider_content')){
+  console.log('blabla')
+  getWidth();
+  window.addEventListener('resize', getWidth);
+}
+function getWidth(){g
   let width = document.documentElement.clientWidth;
   if(width<=900){
     hideSlides();
     showSlide(0);
-    // return 'mobile';
   } else {
     for (i = 0; i < slides.length; i++) {
       slides[i].classList.remove('hidden');
       slides[i].classList.remove('shown');
     }
-    // return 'desktop';
   }
 };
-getWidth();
-window.addEventListener('resize', getWidth);
